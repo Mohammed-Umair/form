811bf53130cda96c7d4c5ec53a37d46559bedd50
Mohammed Umair <94831461+Mohammed-Umair@users.noreply.github.com>
2022-09-24 15:50:30 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import { CreateEvent } from './Components/CreateEvent';
-import { EventDisplay } from './Components/EventDisplay';
-import { Home } from './Components/Home';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { CreateEvent } from "./Components/CreateEvent";
+import { EventDisplay } from "./Components/EventDisplay";
+import { Home } from "./Components/Home";
 
 function App() {
   return (
     <div className="App">
-      
-     <Routes>
-      <Route path='/' element={<Home/>}/> 
-      <Route path='/create' element={<CreateEvent/>}/> 
-      <Route path='/event' element={<EventDisplay/>}/> 
-     </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<CreateEvent />} />
+        <Route path="/event" element={<EventDisplay />} />
+      </Routes>
     </div>
   );
 }

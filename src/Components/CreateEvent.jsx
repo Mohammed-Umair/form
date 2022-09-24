c9a979b20734c302b18d27611ef5d58ddf831de6
Mohammed Umair <94831461+Mohammed-Umair@users.noreply.github.com>
2022-09-25 00:56:38 +0530

    update

--- src/Components/CreateEvent.jsx
+++ src/Components/CreateEvent.jsx
@@ -1,92 +1,136 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./Styles/create.css"
+import "./Styles/create.css";
 
 export const CreateEvent = () => {
-  const [dataa, setDataa] = useState(JSON.parse(localStorage.getItem("event")));
-  const [data, setData] = useState({
-    eventName: "",
-    hostName: "",
-    stratDate: "",
-    endDate: "",
-    adderss: "",
-  });
+  const [eventName, setEventName] = useState();
+  const [hostName, setHostName] = useState();
+  const [startDate, setStratDate] = useState();
+  const [endDate, setEndDate] = useState();
+  const [adderss, setAdderss] = useState();
+  const [file, setFile] = useState();
+
+  useEffect(() => {
+    let ename = localStorage.getItem("event-name");
+    let hname = localStorage.getItem("host-event");
+    let sdate = localStorage.getItem("sdate");
+    let edate = localStorage.getItem("edate");
+    let addres = localStorage.getItem("address");
+    let ffile = localStorage.getItem("files");
+
+    if (ename?.length > 0) {
+      setEventName(ename);
+    }
+    if (hname?.length > 0) {
+      setHostName(hname);
+    }
+    if (sdate?.length > 0) {
+      setStratDate(sdate);
+    }
+    if (edate?.length > 0) {
+      setEndDate(edate);
+    }
+    if (addres?.length > 0) {
+      setAdderss(addres);
+    }
+    if (ffile?.length > 0) {
+      setFile(ffile);
+    }
+  }, []);
 
   const nav = useNavigate();
   const saveEevnt = (e) => {
     e.preventDefault();
 
-    localStorage.setItem("event", JSON.stringify(data));
+    localStorage.setItem("event-name", eventName);
+    localStorage.setItem("host-event", hostName);
+    localStorage.setItem("sdate", startDate);
+    localStorage.setItem("edate", endDate);
+    localStorage.setItem("address", adderss);
+    localStorage.setItem("files", file);
 
     nav("/event");
   };
   return (
     <div className="form-container">
-    <div className="container">
-      <form action="">
-        <div>
-          <label htmlFor="">Event Name</label>
-          <input
-            type="text"
-            name=""
-            value={data.eventName}
-            id=""
-            onChange={(e) => {
-              setData({ ...data, eventName: e.target.value });
-            }}
-          />
-        </div>
-        <div>
-          <label htmlFor="">host name</label>
-          <input
-            type="text"
-            name=""
-            value={data.hostName}
-            id=""
-            onChange={(e) => {
-              setData({ ...data, hostName: e.target.value });
-            }}
-          />
-        </div>
-        <div>
-          <label htmlFor="">event start date</label>
-          <input
-            type="datetime-local"
-            name=""
-            value={data.stratDate}
-            onChange={(e) => {
-              setData({ ...data, stratDate: e.target.value });
-            }}
-            id=""
-          />
-        </div>
-        <div>
-          <label htmlFor="">event end date</label>
-          <input
-            type="datetime-local"
-            value={data.endDate}
-            onChange={(e) => {
-              setData({ ...data, endDate: e.target.value });
-            }}
-            name=""
-            id=""
-          />
-        </div>
-        <div>
-          <label htmlFor="">Addres</label>
-          <input
-            type="address"
-            value={data.adderss}
-            onChange={(e) => {
-              setData({ ...data, adderss: e.target.value });
-            }}
-            name=""
-            id=""
-          />
-        </div>
-        <button onClick={(e) => saveEevnt(e)}>click</button>
-      </form>
-    </div>
+      <div className="container">
+        <form action="">
+          <div className="inputs">
+            <label htmlFor="">Event Name</label>
+            <input
+              type="text"
+              name=""
+              value={eventName}
+              id=""
+              onChange={(e) => {
+                setEventName(e.target.value);
+              }}
+            />
+          </div>
+          <div className="inputs">
+            <label htmlFor="">Host Name</label>
+            <input
+              type="text"
+              name=""
+              value={hostName}
+              id=""
+              onChange={(e) => {
+                setHostName(e.target.value);
+              }}
+            />
+          </div>
+          <div className="inputs">
+            <label htmlFor="">Event Start Date</label>
+            <input
+              type="datetime-local"
+              name=""
+              value={startDate}
+              onChange={(e) => {
+                setStratDate(e.target.value);
+              }}
+              id=""
+            />
+          </div>
+          <div className="inputs">
+            <label htmlFor="">Event End Date</label>
+            <input
+              type="datetime-local"
+              value={endDate}
+              onChange={(e) => {
+                setEndDate(e.target.value);
+              }}
+              name=""
+              id=""
+            />
+          </div>
+          <div className="inputs">
+            <label htmlFor="">Addres</label>
+            <input
+              type="address"
+              value={adderss}
+              onChange={(e) => {
+                setAdderss(e.target.value);
+              }}
+              name=""
+              id=""
+            />
+          </div>
+          <div className="inputs">
+            <label htmlFor="">Event Photo</label>
+            <input
+              type="file"
+              value={file}
+              onChange={(e) => {
+                setFile(e.target.value);
+              }}
+              name=""
+              id=""
+
+            />
+          </div>
+          <button onClick={(e) => saveEevnt(e)}>Next</button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/Components/EventDisplay.jsx
+++ src/Components/EventDisplay.jsx
@@ -1,28 +1,62 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import moment from "moment";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import "./Styles/event.css";
 
 export const EventDisplay = () => {
-    const [data ,setData]=useState()
-    useEffect(() => {
-        setData(JSON.parse(localStorage.getItem('event')))
-    
-     
-    }, [])
-    const nav = useNavigate()
+  const [eventName, seteventName] = useState();
+  const [hostName, sethostName] = useState();
+  const [stratDate, setstratDate] = useState();
+  const [endDate, setendDate] = useState();
+  const [adderss, setadderss] = useState();
+  const [file, setFile]= useState();
+  useEffect(() => {
+    seteventName(localStorage.getItem("event-name"));
+    sethostName(localStorage.getItem("host-event"));
+    setstratDate(localStorage.getItem("sdate"));
+    setendDate(localStorage.getItem("edate"));
+    setadderss(localStorage.getItem("address"));
+    setFile(localStorage.getItem("files "));
+  }, []);
+  const nav = useNavigate();
 
-    const handelEdit =()=>{
-        nav('/create')
-    }
-    
+  const handelEdit = () => {
+    nav("/create");
+  };
+
   return (
-    <div>
-        <h1>{data?.eventName}</h1>
-        <p>hosted by {data?.hostName}</p>
-        <p> {moment(data?.stratDate).format('D MMMM, h:mma')}</p>
-        <p>to {moment(data?.endDate).format('D MMMM, h:mma')}</p>
-        <p><strong>Street Name </strong> <br/> {data?.adderss}</p>
-        {/* <button onClick={()=>{handelEdit()}} >edit</button> */}
+    <div className="e-wrapper">
+      <div className="e-left">
+        <div className="name">
+          <h1 className="eName">{eventName}</h1>
+        </div>
+        <div className="by">
+          <p className="eBy">Hosted by {hostName}</p>
+        </div>
+        <div className="dates">
+          <p className="start"> {moment(stratDate).format("D MMMM, h:mma")}</p>
+          <p className="end">
+            <span>to</span> {moment(endDate).format("D MMMM, h:mma")}
+          </p>
+        </div>
+        <div className="add">
+          <span className="streetLabel">
+            Street Name <br />
+          </span>
+          <span className="street">{adderss}</span>
+        </div>
+        <div className="image">
+          <img src={file} alt="img"  />
+        </div>
+        {/* <button
+          onClick={() => {
+            handelEdit();
+          }}
+        >
+          Edit
+        </button> */}
+      </div>
+      <div className="e-right"></div>
     </div>
-  )
-}
+  );
+};
